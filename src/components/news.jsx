50dd73dfd56b9571916a1f8906e30731547f9789
oshiro17 <nonokapiano@icloud.com>
2023-02-20 13:08:45 +0900

    make info arry

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,43 +1,20 @@
 import React from "react";
 
-const animals = ["inu", "neko", "kuzira"];
+const infos = [{ date: 2020, title: "went to picnic" }, { date: 2021, title: "wake up" }];
 const News = () => {
-  const animal_list = animals.map((animal) => <dt>{animal}</dt>)
   return (
     <div id="news" className="wrapper">
       <h2>News</h2>
       <dl>
-        <div className="first-div">
-        {animal_list}
-          <dd>雑誌から賞をもらいました。</dd>
-        </div>
-        <div>
-          <dt>10XX年</dt>
-          <dd>何かありました</dd>
-        </div>
-        <div>
-          <dt>20JJ年</dt>
-          <dd>ああああああ</dd>
-        </div>
-        <div>
-          <dt>20JJ年</dt>
-          <dd>賞受賞</dd>
-        </div>
-        <div>
-          <dt>20JJ年</dt>
-          <dd>お知らせ！！！わーい</dd>
-        </div>
-        <div>
-          <dt>20JJ年</dt>
-          <dd>っっっっっっっk</dd>
-        </div>
-        <div>
-          <dt>20JJ年</dt>
-          <dd>察しsdsdsdsdし</dd>
-        </div>
+        {infos.map((info, index) => (
+          <div className={index === 0 && "first-div"}>
+            <dt>{info.date}</dt>
+            <dd>{info.title}</dd>
+          </div>))
+        }
       </dl>
-    </div>
+    </div >
 
-    )
+  )
 }
 export default News;
