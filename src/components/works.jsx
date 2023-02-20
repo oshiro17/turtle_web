dbc042be8ae02952fe919b48065fac06e592688c
oshiro17 <nonokapiano@icloud.com>
2023-02-20 13:44:50 +0900

    make images arry

--- src/components/works.jsx
+++ src/components/works.jsx
@@ -1,32 +1,19 @@
 import React from "react";
 import turtle from '../download.svg';
 
-function Works() {
+const images = [turtle, turtle, turtle, turtle, turtle, turtle]
+const image = images.map((image) => 
+<li>
+  <img src={image} alt="" className="img-item" />
+</li>)
+const Works = () => {
   return (
-    <div id="works" className="wrapper">
-      <h2>Works</h2>
-      <ul>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-        <li>
-          <img src={turtle} className="img-item" />
-        </li>
-
-      </ul>
-    </div>
+  <div id="works" className="wrapper">
+    <h2>Works</h2>
+    <ul>
+      {image}
+    </ul>
+  </div>
   )
 }
-export default Works;
+  export default Works;
